25a8225265d3a2b21f0fddb62c5f97f0640f3ec9
kormuhinip <zero14244@Yandex.ru>
2020-01-26 21:59:11 +0400

    create application

---
+++ src/main/java/com/test/task/controller/MessageController.java
@@ -0,0 +1,60 @@
+package com.test.task.controller;
+
+
+import com.google.common.collect.Lists;
+import com.test.task.excepsions.NotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    private int counter = 4;
+    private List<Map<String, String>> messages = Lists.newArrayList(
+            new HashMap<String, String>() {{
+                put("id", "1");
+                put("text", "First message");
+            }},
+            new HashMap<String, String>() {{
+                put("id", "2");
+                put("text", "Second message");
+            }},
+            new HashMap<String, String>() {{
+                put("id", "3");
+                put("text", "Third message");
+            }});
+
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return messages.stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+        messages.add(message);
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDb = getMessage(message.get("id"));
+        messageFromDb.putAll(message);
+    }
+
+}
